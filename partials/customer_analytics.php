3b4a916363573f4819e6d143511d44324707e73d
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-31 20:57:10 +0300

    Craft Customer Analytics

---
+++ partials/customer_analytics.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * Created on Sun Oct 31 2021
+ *
+ *  MartDevelopers Inc - martdev.info 
+ *
+ * mail@martdev.info
+ *
+ * +254 740 847 563
+ *
+ * The MartDevelopers Inc End User License Agreement
+ *
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * 1. GRANT OF LICENSE
+ * MartDevelopers Inc hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ * install and activate this system on two separated computers solely for your personal and non-commercial use,
+ * unless you have purchased a commercial license from MartDevelopers. Sharing this Software with other individuals, 
+ * or allowing other individuals to view the contents of this Software, is in violation of this license.
+ * You may not make the Software available on a network, or in any way provide the Software to multiple users
+ * unless you have first purchased at least a multi-user license from MartDevelopers.
+ *
+ * 2. COPYRIGHT 
+ * The Software is owned by MartDevelopers and protected by copyright law and international copyright treaties. 
+ * You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ * 3. RESTRICTIONS ON USE
+ * You may not, and you may not permit others to
+ * (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ * (b) modify, distribute, or create derivative works of the Software;
+ * (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ * otherwise exploit the Software.  
+ *
+ * 4. TERM
+ * This License is effective until terminated. 
+ * You may terminate it at any time by destroying the Software, together with all copies thereof.
+ * This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ * Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ * 5. NO OTHER WARRANTIES. 
+ * MartDevelopers Inc  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ * MartDevelopers Inc SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ * SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ * ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ * THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ * HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ * 6. SEVERABILITY
+ * In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ * affect the validity of the remaining portions of this license.
+ *
+ * 7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL MartDevelopers Inc  OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ * CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ * USE OF THE SOFTWARE, EVEN IF MartDevelopers Inc HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ * IN NO EVENT WILL MartDevelopers Inc  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ * TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ */
+
+$user_id = $_SESSION['user_id'];
+
+/* Overall sales  */
+$query = "SELECT SUM(payment_amount) FROM payment p
+     INNER JOIN cart c ON c.cart_id = p.payment_cart_id
+     WHERE c.cart_user_id = '$user_id'";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($revenue);
+$stmt->fetch();
+$stmt->close();
+
+
+
+/* Pending Orders */
+$query = "SELECT COUNT(*) FROM cart 
+WHERE cart_checkout_status = 'Pending' AND cart_user_id = '$user_id'";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($pending_orders);
+$stmt->fetch();
+$stmt->close();
+
+$query = "SELECT COUNT(*) FROM cart 
+WHERE cart_checkout_status = 'Paid' AND cart_user_id = '$user_id'";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($paid_orders);
+$stmt->fetch();
+$stmt->close();
