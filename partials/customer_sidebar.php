907a4f8038b8ed23213305f1bc846c266d06e515
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-31 20:54:37 +0300

    Segment Landing Page Header & Navigation bar

---
+++ partials/customer_sidebar.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ * Created on Sun Oct 31 2021
+ *
+ *  MartDevelopers Inc - martdev.info 
+ *
+ * mail@martdev.info
+ *
+ * +254 740 847 563
+ *
+ * The MartDevelopers Inc End User License Agreement
+ *
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * 1. GRANT OF LICENSE
+ * MartDevelopers Inc hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ * install and activate this system on two separated computers solely for your personal and non-commercial use,
+ * unless you have purchased a commercial license from MartDevelopers. Sharing this Software with other individuals, 
+ * or allowing other individuals to view the contents of this Software, is in violation of this license.
+ * You may not make the Software available on a network, or in any way provide the Software to multiple users
+ * unless you have first purchased at least a multi-user license from MartDevelopers.
+ *
+ * 2. COPYRIGHT 
+ * The Software is owned by MartDevelopers and protected by copyright law and international copyright treaties. 
+ * You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ * 3. RESTRICTIONS ON USE
+ * You may not, and you may not permit others to
+ * (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ * (b) modify, distribute, or create derivative works of the Software;
+ * (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ * otherwise exploit the Software.  
+ *
+ * 4. TERM
+ * This License is effective until terminated. 
+ * You may terminate it at any time by destroying the Software, together with all copies thereof.
+ * This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ * Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ * 5. NO OTHER WARRANTIES. 
+ * MartDevelopers Inc  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ * MartDevelopers Inc SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ * SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ * ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ * THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ * HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ * 6. SEVERABILITY
+ * In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ * affect the validity of the remaining portions of this license.
+ *
+ * 7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL MartDevelopers Inc  OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ * CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ * USE OF THE SOFTWARE, EVEN IF MartDevelopers Inc HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ * IN NO EVENT WILL MartDevelopers Inc  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ * TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ */
+
+/* Pop System Settings Here */
+$user_id = $_SESSION['user_id'];
+$ret = "SELECT * FROM  users s 
+JOIN  system_settings  
+WHERE s.user_id = '$user_id'";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+    /* Check User Has Profile Image */
+    if ($sys->user_dpic == '') {
+        $img = "../public/backend_assets/images/avatars/no-profile.png";
+    } else {
+        $img = "../public/backend_assets/images/avatars/$sys->user_dpic";
+    }
+
+?>
+    <div class="app-sidebar">
+        <div class="logo">
+            <a href="home" class="logo-icon"><span class="logo-text"><?php echo $sys->sys_name; ?></span></a>
+            <div class="sidebar-user-switcher user-activity-online">
+                <a href="farmer_profile">
+                    <img src="<?php echo $img; ?>"><br><br><br>
+                    <span class="user-info-text">
+                        <?php echo $sys->user_name; ?>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="app-menu">
+            <ul class="accordion-menu">
+                <li>
+                    <a href="customer_home" class="active"><i class="material-icons-two-tone">dashboard</i>Dashboard</a>
+                </li>
+
+                <li>
+                    <a href="customer_products"><i class="material-icons-two-tone">redeem</i>Agri Products</a>
+                </li>
+                <li>
+                    <a href=""><i class="material-icons-two-tone">shopping_cart</i>Sales<i class="material-icons has-sub-menu">keyboard_arrow_right</i></a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="customer_pending_checkouts">Pending Checkouts</a>
+                        </li>
+                        <li>
+                            <a href="customer_successful_checkouts">Successful Checkouts</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="customer_payments"><i class="material-icons-two-tone">price_check</i>Payments</a>
+                </li>
+                <li>
+                    <a href="logout"><i class="material-icons-two-tone">logout</i>Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+<?php } ?>
