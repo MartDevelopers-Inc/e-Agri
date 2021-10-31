dd20868f7b86c35a7e84fa067b71a2a92578acd3
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-31 08:13:43 +0300

    Craft Farmer Analytics

---
+++ partials/farmer_header.php
@@ -0,0 +1,27 @@
+<div class="app-header">
+    <nav class="navbar navbar-light navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="navbar-nav" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link hide-sidebar-toggle-button" href="#"><i class="material-icons">first_page</i></a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="d-flex">
+                <ul class="navbar-nav">
+                    <li class="nav-item hidden-on-mobile">
+                        <a class="nav-link" href="farmer_profile">Profile</a>
+                    </li>
+                    <li class="nav-item hidden-on-mobile">
+                        <a class="nav-link" href="farmer_reports">Reports</a>
+                    </li>
+                    <li class="nav-item hidden-on-mobile">
+                        <a class="nav-link" href="logout">Log Out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
